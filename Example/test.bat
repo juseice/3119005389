3c464257f2e992fe175095d6d8125feef0771227
juseice <59499786+juseice@users.noreply.github.com>
2021-09-23 11:51:00 +0800

    Update test.bat
    The test id is wrong and update now.

--- Example/test.bat
+++ Example/test.bat
@@ -14,15 +14,15 @@ rem test 2:
 C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\f2.txt C:\Users\juseice\Desktop\streamtest\s2.txt C:\Users\juseice\Desktop\streamtest\ans2.txt
 rem test 3:
 C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\f3.txt C:\Users\juseice\Desktop\streamtest\s3.txt C:\Users\juseice\Desktop\streamtest\ans3.txt
-rem test 3:
-C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_add.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_add.txt
 rem test 4:
-C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_del.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_del.txt
+C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_add.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_add.txt
 rem test 5:
-C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_dis_1.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_dis_1.txt
+C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_del.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_del.txt
 rem test 6:
-C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_dis_10.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_dis_10.txt
+C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_dis_1.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_dis_1.txt
 rem test 7:
+C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_dis_10.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_dis_10.txt
+rem test 8:
 C:\Users\juseice\Desktop\streamtest\main.exe C:\Users\juseice\Desktop\streamtest\orig.txt C:\Users\juseice\Desktop\streamtest\orig_0.8_dis_15.txt C:\Users\juseice\Desktop\streamtest\ans_orig_0.8_dis_15.txt
 
 pause
