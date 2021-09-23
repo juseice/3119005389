054014789c1bfc2fc052b70b74055df37c9e0256
juseice <59499786+juseice@users.noreply.github.com>
2021-09-23 11:51:24 +0800

    Update test_local.bat
    The test id is wrong and update now.

--- Example/test_local.bat
+++ Example/test_local.bat
@@ -14,15 +14,15 @@ rem test 2:
 main.exe f2.txt s2.txt ans2.txt
 rem test 3:
 main.exe f3.txt s3.txt ans3.txt
-rem test 3:
-main.exe orig.txt orig_0.8_add.txt ans_orig_0.8_add.txt
 rem test 4:
-main.exe orig.txt orig_0.8_del.txt ans_orig_0.8_del.txt
+main.exe orig.txt orig_0.8_add.txt ans_orig_0.8_add.txt
 rem test 5:
-main.exe orig.txt orig_0.8_dis_1.txt ans_orig_0.8_dis_1.txt
+main.exe orig.txt orig_0.8_del.txt ans_orig_0.8_del.txt
 rem test 6:
-main.exe orig.txt orig_0.8_dis_10.txt ans_orig_0.8_dis_10.txt
+main.exe orig.txt orig_0.8_dis_1.txt ans_orig_0.8_dis_1.txt
 rem test 7:
+main.exe orig.txt orig_0.8_dis_10.txt ans_orig_0.8_dis_10.txt
+rem test 8:
 main.exe orig.txt orig_0.8_dis_15.txt ans_orig_0.8_dis_15.txt
 
 pause
